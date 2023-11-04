93495de794a1a173f74506c864ceea2b82fe0cd3
Carbon225 <carbon225@protonmail.com>
2023-11-04 17:46:53 +0100

    joy

--- scripts/joycmd.py
+++ scripts/joycmd.py
@@ -5,20 +5,24 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
 
-def main():
-    rospy.init_node('control_node')
+def threshold(x, low=0.1):
+    return x if abs(x) >= low else 0
 
-    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=4)
+
+def main():
+    rospy.init_node('joycmd')
+
+    cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
     def on_joy(data: Joy):
         rospy.logdebug(rospy.get_caller_id() + f"[joy] {data.axes}")
         msg = Twist()
-        msg.linear.x = data.axes[1]
-        msg.linear.y = data.axes[0]
-        msg.angular.z = data.axes[2]
+        msg.linear.x = threshold(data.axes[1])
+        msg.linear.y = threshold(data.axes[0])
+        msg.angular.z = threshold(data.axes[3])
         cmd_vel_pub.publish(msg)
 
-    rospy.Subscriber("joy", Joy, on_joy, queue_size=8)
+    rospy.Subscriber("joy", Joy, on_joy, queue_size=1)
 
     rospy.spin()
 
