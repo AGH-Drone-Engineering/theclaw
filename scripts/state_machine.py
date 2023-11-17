2e5ffb53d087a789c1b8de734d07995399775d79
DK333D <64826011+DK333D@users.noreply.github.com>
2023-11-17 18:57:29 +0100

    Update state_machine.py
    Check if state state changed

--- scripts/state_machine.py
+++ scripts/state_machine.py
@@ -13,6 +13,7 @@ class StateMachine:
     def __init__(self):
        
         self.CURRENT_STATE = IdStan.SZUKAJ_MIS
+        self.PREVIOUS_STATE = IdStan.SZUKAJ_MIS
 
         rospy.init_node('teddy_subscriber', anonymous=True)  
         self.stan_sub = rospy.Subscriber("teddy_topic", Int32, self.callback_state)  
@@ -22,20 +23,20 @@ class StateMachine:
         self.teddy_pub.publish(teddy_msg)
 
     def main_loop(self):
+
+
         while not rospy.is_shutdown():
             #chodzeie do misia
             if self.CURRENT_STATE == IdStan.SZUKAJ_MIS:
                 self.seek_bear()
                 teddy_msg = Int32()
                 teddy_msg.data = IdStan.IDZ_DO_MIS
-                self.teddy_pub.publish(teddy_msg)
             
             #chwytanie misia
             elif self.CURRENT_STATE == IdStan.IDZ_DO_MIS:
                 self.go_to_bear()
                 teddy_msg = Int32()
                 teddy_msg.data = IdStan.CHWYT
-                self.teddy_pub.publish(teddy_msg)
             
             #chwyt misia
 
@@ -43,7 +44,6 @@ class StateMachine:
                 self.catch_bear()
                 teddy_msg = Int32()
                 teddy_msg.data = IdStan.UCIEKAJ
-                self.teddy_pub.publish(teddy_msg)
             
             #ucieczka 
             
@@ -51,7 +51,11 @@ class StateMachine:
                 self.run_away()
                 teddy_msg = Int32()
                 teddy_msg.data = IdStan.SZUKAJ_MIS
-                self.teddy_pub.publish(teddy_msg)
+
+
+            self.teddy_pub.publish(teddy_msg)
+            # save previous state
+            self.PREVIOUS_STATE = self.CURRENT_STATE
 
     def seek_bear(self):
         goToBearArea()
@@ -83,15 +87,16 @@ class StateMachine:
         print("Uciekaj!")
         goHome()
         return None
-        
+    
     def callback(self, data):
+        if self.PREVIOUS_STATE != self.CURRENT_STATE:
+            rospy.loginfo("Otrzymano: %d", data.data)
         # rospy.loginfo("Otrzymano: %d", data.data)  # Wyświetlenie odebranej wiadomości
         # Tutaj umieść logikę obsługi odebranej wiadomości (np. zmiana flagi bear_found)
         if data.data == 1:  
             teddy_msg = Int32()
             teddy_msg.data = self.CURRENT_STATE
             self.teddy_pub.publish(teddy_msg)
-
 
 if __name__ == "__main__":
     state_machine = StateMachine()
