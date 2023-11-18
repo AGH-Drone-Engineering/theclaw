ab40afe7c2bcd1e5a7d13fa36838517dd12b7c38
Carbon225 <carbon225@protonmail.com>
2023-11-18 09:57:24 +0100

    xd

--- scripts/joycmd.py
+++ scripts/joycmd.py
@@ -3,36 +3,41 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Float64
+from std_msgs.msg import UInt8
+
+
+SERVO_OPEN = 10
+SERVO_CLOSE = 70
 
 
 def threshold(x, low=0.05):
     return x if abs(x) >= low else 0
 
 
+def rescale(x, fromlo, fromhi, tolo, tohi):
+    return (x - fromlo) / (fromhi - fromlo) * (tohi - tolo) + tolo
+
+
 def main():
     rospy.init_node('joycmd')
 
     cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-    gripper_left_pub = rospy.Publisher('gripper_arm_left_controller/command', Float64, queue_size=1)
-    gripper_right_pub = rospy.Publisher('gripper_arm_right_controller/command', Float64, queue_size=1)
+    gripper_pub = rospy.Publisher('hw/gripper/command', UInt8, queue_size=1)
     last_msg = Twist()
 
     def on_joy(data: Joy):
         rospy.logdebug(rospy.get_caller_id() + f"[joy] {data.axes}")
         msg = Twist()
-        msg.linear.x = threshold(data.axes[1]) * 0.5
-        msg.angular.z = threshold(data.axes[3]) * 2.0
+        msg.linear.x = threshold(data.axes[1])
+        msg.angular.z = threshold(data.axes[3])
         cmd_vel_pub.publish(msg)
         nonlocal last_msg
         last_msg = msg
 
-        msg_left = Float64()
-        msg_right = Float64()
+        msg = UInt8()
         gripper_trigger = data.axes[5]
-        msg_left.data = msg_right.data = gripper_trigger + 0.5
-        gripper_left_pub.publish(msg_left)
-        gripper_right_pub.publish(msg_right)
+        msg.data = int(rescale(gripper_trigger, -1, 1, SERVO_OPEN, SERVO_CLOSE))
+        gripper_pub.publish(msg)
 
     rospy.Subscriber("joy", Joy, on_joy, queue_size=64)
 
